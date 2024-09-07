9dd79c07d74e5a6fb3ce8eea1f91249163634d8a
erizkaaaaaa <147624260+erizkanr@users.noreply.github.com>
2024-09-07 09:22:28 +0700

    Pertemuan1

---
+++ praktikum1/Pertemuan1/MatematikaDemo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Pertemuan1;
+
+/**
+ *
+ * @author abdul rohim
+ */
+public class MatematikaDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Matematika mtk = new Matematika();
+        
+        mtk.a = 13;
+        mtk.b = 2;
+        mtk.info();
+    }
+    
+}
---
+++ praktikum1/Pertemuan1/sepeda.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan1;
+
+/**
+ *
+ * @author abdul rohim
+ */
+public class sepeda {
+    String warna;
+    int tahunPrduksi;
+    
+}
