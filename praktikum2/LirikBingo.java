9fa55c3a9b946e93d19fcae7f1a7692cf4607b7d
erizkaaaaaa <147624260+erizkanr@users.noreply.github.com>
2024-09-10 21:47:32 +0700

    pertemuan2

---
+++ praktikum2/LirikBingo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktikum2;
+
+/**
+ *
+ * @author abdul rohim
+ */
+public class LirikBingo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String lirik = null;
+        lirik ll = new lirik(lirik);
+        ll.info();
+    }
+    
+}
